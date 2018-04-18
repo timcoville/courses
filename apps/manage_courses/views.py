382c1cef98f55e4f7d46229e1ddf60205e59843b
Tim Coville <tcoville@gmail.com>
2018-04-18 15:01:10 -0700

    Added Models, Add New Course Function, and syntax for displaying course on index.html

--- apps/manage_courses/views.py
+++ apps/manage_courses/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from models import *
 
 def index(request):
-    print "It works"
-    return render(request, "manage_courses/index.html")
+    context = { 'courses': Course.objects.all()}
+    return render(request, "manage_courses/index.html", context)
 
 def create(request):
-    return HttpResponse('We created')
+    errors = Course.objects.basic_validator(request.POST)
+    if len(errors):
+        for tag, error in errors.iteritems():
+            messages.error(request, error, extra_tags=tag)
+            print tag + ": " + error
+        return redirect('/')
+    else:
+        Course.objects.create(name=request.POST['name'],desc=request.POST['desc'])
+    return redirect('/')
 
 def remove(request, id):
     print id
